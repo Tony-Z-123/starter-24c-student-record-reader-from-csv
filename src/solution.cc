4cc5b5f7cf2372b991e15fffdc6694aed5ea77b5
Tony-Z-123 <tonyz5@illinois.edu>
2026-02-11 15:34:52 -0600

    Add ReadStudentRecFromStream implementation
    Implement ReadStudentRecFromStream to read student data from input stream.

--- src/solution.cc
+++ src/solution.cc
@@ -1,6 +1,31 @@
 #include "solution.hpp"
+#include "student.hpp"
+#include <fstream>
+#include <string>
 
 Student ReadStudentRecFromStream(std::istream& is) {
-  // your implementation here...
-  return Student{};
+    std::string first;
+    std::string last_with_comma;
+
+    if (!(is >> first >> last_with_comma))
+        return Student{};
+
+    if (last_with_comma.empty() || last_with_comma.back() != ',')
+        return Student{};
+
+    last_with_comma.pop_back();
+
+    unsigned int uin;
+    if (!(is >> uin))
+        return Student{};
+
+    char comma;
+    if (!(is >> comma) || comma != ',')
+        return Student{};
+
+    double gpa;
+    if (!(is >> gpa))
+        return Student{};
+
+    return Student(first + " " + last_with_comma, uin, gpa);
 }
